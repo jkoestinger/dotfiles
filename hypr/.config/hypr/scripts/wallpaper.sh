8e249fc1493fd5dfc245099e7636a1ab2e286aee
Justin Koestinger <justin.koestinger@proton.me>
2024-07-23 21:40:11 +0200

    fix scripts

--- hypr/.config/hypr/scripts/wallpaper.sh
+++ hypr/.config/hypr/scripts/wallpaper.sh
@@ -16,7 +16,7 @@ rasi_file="$HOME/.cache/current_wallpaper.rasi"
 blur_file="$HOME/.config/blur.sh"
 
 blur="50x30"
-blur=$(cat $blur_file)
+# blur=$(cat $blur_file)
 
 # Create cache file if not exists
 if [ ! -f $cache_file ] ;then
