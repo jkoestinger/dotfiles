eb3faa187c56665c93fddcbf56b08e4f30d86dcc
Justin Koestinger <justin.koestinger@proton.me>
2025-08-26 19:41:06 +0200

    feat: add local config setup

--- install.sh
+++ install.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+HYPR_LOCAL_PATH=$HOME/.config/hypr-local
+
 echo [*] Installing zsh...
 yay -S zsh zsh-antidote
 sudo chsh -s /usr/bin/zsh $USER
+
+echo [*] Creating local hypr config files...
+mkdir -p $HYPR_LOCAL_PATH
+touch $HYPR_LOCAL_PATH/autostart.conf
+touch $HYPR_LOCAL_PATH/bindings.conf
+touch $HYPR_LOCAL_PATH/envs.conf
+touch $HYPR_LOCAL_PATH/hypridle.conf
+touch $HYPR_LOCAL_PATH/hyprland.conf
+touch $HYPR_LOCAL_PATH/hyprlock.conf
+touch $HYPR_LOCAL_PATH/hyprsunset.conf
+touch $HYPR_LOCAL_PATH/input.conf
+touch $HYPR_LOCAL_PATH/monitors.conf
+touch $HYPR_LOCAL_PATH/windows.conf
 
 echo [*] Installing configs...
 stow -S nvim
